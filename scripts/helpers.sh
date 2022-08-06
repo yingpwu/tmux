3750a554bf3ff3da51c077a95bee7371a83b7a2a
yingpwu <46162933+yingpwu@users.noreply.github.com>
2022-08-06 15:34:42 +0800

    support tmux-net-speed

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,36 +1,112 @@
 #!/usr/bin/env bash
 
+##
+# Varialbes
+##
 PATH="/usr/local/bin:$PATH:/usr/sbin"
-
-set_tmux_option() {
-  local option_name="$1"
-  local option_value="$2"
-  $(tmux set-option -gq $option_name "$option_value")
-}
-
-os_type() {
-  local os_name="unknown"
-
-  case $(uname | tr '[:upper:]' '[:lower:]') in
-    linux*)
-      os_name="linux"
-      ;;
-    darwin*)
-      os_name="osx"
-      ;;
-    msys*)
-      os_name="windows"
-      ;;
-    freebsd*)
-      os_name="freebsd"
-      ;;
-  esac
-
-  echo -n $os_name
-}
+DOWNLOAD_FILE="/tmp/tmux_net_speed.download"
+UPLOAD_FILE="/tmp/tmux_net_speed.upload"
 
 export LANG=C
 export LC_ALL=C
+
+get_velocity()
+{
+    local new_value=$1
+    local old_value=$2
+
+    # Consts
+    local THOUSAND=1024
+    local MILLION=1048576
+
+    local interval=$(get_tmux_option 'status-interval' 5)
+    local vel=$(( ( new_value - old_value ) / interval ))
+    local vel_kb=$(( vel / THOUSAND ))
+    local vel_mb=$(( vel / MILLION ))
+
+    if [[ $vel_mb != 0 ]] ; then
+        echo -n "$vel_mb MB/s"
+    elif [[ $vel_kb != 0 ]] ; then
+        echo -n "$vel_kb KB/s";
+    else
+        echo -n "$vel B/s";
+    fi
+}
+
+# Reads from value from file. If file does not exist,
+# is empty, or not readable, starts back at 0
+read_file()
+{
+    local path="$1"
+    local fallback_val=0
+
+    # File exists and is readdable?
+    if [[ ! -f "$path" ]] ; then
+        echo $fallback_val
+        return 1
+    elif [[ ! -r "$path" ]]; then
+        echo $fallback_val
+        return 1
+    fi
+
+
+    # Does the file have content?
+    tmp=$(< "$path")
+    if [[ "x${tmp}" == "x" ]] ; then
+        echo $fallback_val
+        return 1
+    fi
+
+    # Now return known value
+    echo $tmp
+}
+
+# Update values in file
+write_file()
+{
+    local path="$1"
+    local val="$2"
+
+    # TODO Add error checking
+    echo "$val" > "$path"
+}
+
+get_interfaces()
+{
+    local interfaces=$(get_tmux_option @net_speed_interfaces "")
+
+    if [[ -z "$interfaces" ]] ; then
+        for interface in /sys/class/net/*; do
+            interfaces+=$(echo $(basename $interface) " ");
+        done
+    fi
+
+    # Do not quote the variable. This way will handle trailing whitespace
+    echo -n $interfaces
+}
+
+sum_speed()
+{
+    local column=$1
+
+    declare -a interfaces=$(get_interfaces)
+
+    local line=""
+    local val=0
+    for intf in ${interfaces[@]} ; do
+        line=$(cat /proc/net/dev | grep "$intf" | cut -d':' -f 2)
+        speed="$(echo -n $line | cut -d' ' -f $column)"
+        let val+=${speed:=0}
+    done
+
+    echo $val
+}
+
+set_tmux_option() {
+    local option=$1
+    local value=$2
+    tmux set-option -gq "$option" "$value"
+}
 
 get_tmux_option() {
   local option
